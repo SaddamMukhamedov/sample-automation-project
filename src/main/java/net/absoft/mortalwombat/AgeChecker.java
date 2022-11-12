2de475966d73bb85d0cfbef38b4ad19d2bf4d44f
Saddam Mukhamedov <free1manlovecs@gmail.com>
2022-11-12 19:38:17 +0200

    Added failing test

--- src/main/java/net/absoft/mortalwombat/AgeChecker.java
+++ src/main/java/net/absoft/mortalwombat/AgeChecker.java
@@ -1,9 +1,9 @@
 package net.absoft.mortalwombat;
 
 public class AgeChecker {
-    private final int MIN_AGE = 18;
+    private final int MIN_AGE = 21;
 
-    public boolean caPlayGame(int age) {
+    public boolean canPlayGame(int age) {
         return age >= this.MIN_AGE;
     }
 }
