815afd67007cf93a1880153e0a6bc29dde75da7d
Saddam Mukhamedov <free1manlovecs@gmail.com>
2022-11-14 20:36:07 +0200

    Added Discriminant Check

--- src/main/java/net/absoft/discriminantCalculator/Solution.java
+++ src/main/java/net/absoft/discriminantCalculator/Solution.java
@@ -12,10 +12,10 @@ public class Solution{
     }
 
     public void RootsSolution() {
-// Discriminant
+
 
         int discriminant = (b * b - 4 * a * c);
-//        Roots result = new Roots();
+
         System.out.println("Discriminant = " + discriminant);
         if (discriminant == 0) {
             Root1 = Root2 = -b / (2 * a);
@@ -32,24 +32,5 @@ public class Solution{
         }
 
     }
-        //    public void solution(int a, int b, int c) {
-//
-//       int discriminant = (b*b -4*a*c);
-//        System.out.println("Discriminant = " + discriminant);
-//        if (discriminant < 0) {
-//            System.out.println("null due to d ==0");
-//
-//        } else if (discriminant == 0) {
-//            System.out.println("Корни: " + searchRootsWhenDiscriminantEqualsToZero(a, b));
-//
-//
-//        } else {
-//            System.out.println(">0");
-//        }
-//
-//
-//
-//    }
-
 
 }
