164a3c61872bb40b5fc115f8362e7a502a86c518
SaddamMukhamedov <116290731+SaddamMukhamedov@users.noreply.github.com>
2023-03-05 14:21:05 +0200

    Update age constant from '18' to '21'
    Update age constant in order to verify that young user can't play a game and the test will fail

--- src/main/java/net/absoft/mortalwombat/AgeChecker.java
+++ src/main/java/net/absoft/mortalwombat/AgeChecker.java
@@ -1,7 +1,7 @@
 package net.absoft.mortalwombat;
 
 public class AgeChecker {
-    private final int MIN_AGE = 18;
+    private final int MIN_AGE = 21;
 
     public boolean caPlayGame(int age) {
         return age >= this.MIN_AGE;
