362d4243e752ae6183d22296e387c861bf7feb09
SaddamMukhamedov <116290731+SaddamMukhamedov@users.noreply.github.com>
2023-03-05 14:16:36 +0200

    Fix reason of failing in testThatTooYoungUsersCanNotPlay()

--- src/test/java/net/absoft/wombat/AgeCheckerTest.java
+++ src/test/java/net/absoft/wombat/AgeCheckerTest.java
@@ -5,22 +5,15 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AgeCheckerTest {
-
-
     @Test
     public void testAgedUserCanPlay() {
-
         AgeChecker ageChecker = new AgeChecker();
         Assert.assertTrue(ageChecker.canPlayGame(25), "Aged user can't play a game");
-
     }
+    
     @Test
     public void testThatTooYoungUsersCanNotPlay() {
-
         AgeChecker ageChecker = new AgeChecker();
-        Assert.assertFalse(ageChecker.canPlayGame(19), "Aged user can't play a game");
-
+        Assert.assertFalse(ageChecker.canPlayGame(19), "Young user can't play a game");
     }
-
-
 }
